40635aa2ff3d1486b24bd33e1d3f1727822f594f
Itawel Oumrou <itawloumrou6@gmail.com>
2022-01-09 16:16:37 +0000

    Ajout des deonnees dans le memoire

--- src/projet/memoire.java
+++ src/projet/memoire.java
@@ -8,14 +8,19 @@ class memoire {
 	private static Map<Long, Directeur> directeurs = new HashMap<>();
 	public static Map<Long, Directeur> getDirecteurs(){
 		directeurs.put((long) 1,new Directeur("Ahmed Jiddou","It",1,"1234"));
-		
+		directeurs.put((long) 2,new Directeur("Ahmed","It",2,"1ws234"));
+		directeurs.put((long) 3,new Directeur("Jiddou","Itawel",3,"123dx4"));
+		directeurs.put((long) 4,new Directeur("Ahmed taleb","Itawel",4,"1234rr"));
 		return directeurs;
 	}
 	
 	//Pour le  caissier
 	private static Map<Long, Caissier> caissiers = new HashMap<>();
 	public static Map<Long, Caissier> getCassiers(){
-		//caissiers.put((Long) 1,new Caissier("AH","IT",1,"0012"));
+		caissiers.put((long) 1,new Caissier("AH","IT",(long) 1,"0012"));
+		caissiers.put((long) 2,new Caissier("Sidi","Nez",(long) 2,"001xc2"));
+		caissiers.put((long) 3,new Caissier("Ahmedou","Am",(long) 3,"0dxxx012"));
+		caissiers.put((long) 4,new Caissier("Diallo","Gabriel",(long) 4,"00ee12"));
 		return caissiers;
 	}
 	
@@ -23,6 +28,9 @@ class memoire {
 		private static Map<Long, Magasin> magasins = new HashMap<>();
 		public static Map<Long, Magasin> getMagasins(){
 			magasins.put((long) 1, new Magasin((long) 1,"Epicerie",(long) 1,null));
+			magasins.put((long) 2, new Magasin((long) 2,"Epicerie 3",(long) 1,null));
+			magasins.put((long) 3, new Magasin((long) 3,"Epicerie 3",(long) 2,null));
+			magasins.put((long) 4, new Magasin((long) 4,"Epicerie 4",(long) 3,null));
 			return magasins;
 		}
 		
@@ -30,6 +38,9 @@ class memoire {
 				private static Map<Long, Produit> produits = new HashMap<>();
 				public static Map<Long, Produit> getProduits(){
 					produits.put((long) 1, new Produit((long) 1,"Biscuit","123oiu4"));
+					produits.put((long) 2, new Produit((long) 2,"Biscuit ser9ella","123odiu4"));
+					produits.put((long) 3, new Produit((long) 3,"Sardine","1234"));
+					produits.put((long) 4, new Produit((long) 4,"Oreo","123o4"));
 					return produits;
 				}
 				
